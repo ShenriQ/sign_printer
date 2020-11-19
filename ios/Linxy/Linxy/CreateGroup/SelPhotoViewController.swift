1acc0385bd0b4540056516c0fb951fe01eba6374
smart <smart@smarts-Mac.local>
2020-11-19 02:04:11 -0800

    select photo, video, almost done page

---
+++ ios/Linxy/Linxy/CreateGroup/SelPhotoViewController.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+class SelPhotoViewController: UIViewController {
+    
+  
+    @IBOutlet weak var tableView: UITableView!
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+    
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+
+}
+
+
+//MARK: UITableView Delegate & DataSource
+extension SelPhotoViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "selphotoTableViewCell")
+        return cell!
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+
+        return 680
+    }
+
+}
+
+
