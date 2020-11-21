b3d377e463d466c2713587e7e4486fcd64b27a93
shendi <shendi@gmail.com>
2020-11-21 19:38:06 +0200

    android fix ui

--- android/linksy/app/src/main/java/com/linxy/linxymark/view/SplashActivity.java
+++ android/linksy/app/src/main/java/com/linxy/linxymark/view/SplashActivity.java
@@ -32,53 +32,6 @@ public class SplashActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_splash);
-        askPersmission();
-    }
-
-
-    public void askPersmission(){
-
-        PermissionListener permissionlistener = new PermissionListener() {
-            @Override
-            public void onPermissionGranted() {
-                new Handler(){
-                    @Override
-                    public void handleMessage(Message msg) {
-                        super.handleMessage(msg);
-//                 Start home activity
-//                        startActivity(new Intent(SplashActivity.this, CustomMainActivity.class));
-//                 close splash activity
-//                        finish();
-                    }
-                }.sendEmptyMessageDelayed(0,2000);
-            }
-
-            @Override
-            public void onPermissionDenied(List<String> deniedPermissions) {
-                showDialog("Alert", "Camera permission not granted! Can not scan!");
-            }
-        };
-
-        TedPermission.with(this)
-                .setPermissionListener(permissionlistener)
-                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
-                .setPermissions(Manifest.permission.CAMERA)
-                .check();
-
-    }
-
-    private void showDialog(String title, String message) {
-        AlertDialog alertDialog = new AlertDialog.Builder(SplashActivity.this).create();
-        alertDialog.setTitle(title);
-        alertDialog.setMessage(message);
-        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        askPersmission();
-                    }
-                });
-        alertDialog.show();
     }
 
     public void onLoginPress(View view)
